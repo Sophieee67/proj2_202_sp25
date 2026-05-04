7e625b7345c7f9cc943aaa00f3848d8d72028dc7
Sophieee67 <k2_sophie@yahoo.com>
2026-05-03 23:39:22 -0700

    Refactor proj2.py with new data classes
    Refactor proj2.py to include future annotations, additional imports, and define Row and Node classes.

--- proj2.py
+++ proj2.py
@@ -1,13 +1,32 @@
+from __future__ import annotations
+import sys
 import csv
-import math
-from dataclasses import dataclass
 from typing import *
-
+from dataclasses import dataclass
+import unittest
+import math
+sys.setrecursionlimit(10_000)
 
 # Put your data definitions first!
-
+@dataclass(frozen=True)
+class Row
+  country: str
+  year: int
+  electricity_and_heat_co2_emissions: float|None
+  electricity_and_heat_co2_emissions_per_capita: float|None
+  energy_co2_emission: float|None
+  energy_co2_emissions_per_capita	total_co2_emissions_excluding_lucf: float|None
+  total_co2_emissions_excluding_lucf_per_capita: float|None
+  
+class Node
+  value = Row
+  next = Node | None
+     
 # ...
 
 # Then your functions.
+
+read_csv_lines(filename: str) -> Optional[Node]
+  
 
 # ...
